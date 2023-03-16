5f56cd56b3c710c03c5d718a4a20ab69cbb943df
Yathip Kenichi Takahashi <ken.yathip@gmail.com>
2023-03-16 22:05:33 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -13,9 +12,17 @@ function App() {
 	};
 
   // Tasks list
-  const [tasks, setTasks] = useState(["Complete online JavaScript course", "Jog around the park 3x", "10 minutes meditation", "Read for 1 hour", "Pick up groceries", "Complete Todo App on Frontend Mentor"]);
-  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState([
+    {name: "Complete online JavaScript course", completed: false},
+    {name: "Jog around the park 3x", completed: false},
+    {name: "10 minutes meditation", completed: false},
+    {name: "Read for 1 hour", completed: false},
+    {name: "Pick up groceries", completed: false},
+    {name: "Complete Todo App on Frontend Mentor", completed: false}
+  ]);
+
   const [checkbox, setCheckbox] = useState(Array(tasks.length).fill(false));
+  const [completedTasks, setCompletedTasks] = useState([]);
 
   const handleCheckboxClick = (index) => {
     setCheckbox((prevCheckbox) => {
@@ -25,19 +32,20 @@ function App() {
     });
   };
 
-  const handleAddTask = () => {
-    if (newTask.trim() === '') {
-      return;
+  const handleAddTask = (e) => {
+    if (e.key === "Enter" && e.target.value !== "") {
+      setTasks([...tasks, {text: e.target.value, complete: false}]);
+      e.target.value = "";
     }
-
-    setTasks((prevTasks) => [...prevTasks, newTask.trim()]);
-    setNewTask('');
-    setCheckbox((prevCheckboxes) => [...prevCheckboxes, false]);
   };
 
-  const handleDelete = (index) => {
+  const handleDelete = (task) => {
+    setTasks(tasks.filter((t) => t !== task));
+    setCompletedTasks(completedTasks.filter((t) => t !== task));
+  };
 
-  }
+
+  
 
   return (
     <div className="App">
@@ -54,27 +62,26 @@ function App() {
         </div>
 
         <div className="create-new">
-            <input
+            <input 
+              onKeyDown={handleAddTask}
               placeholder="Create a new todo..."
             />
         </div>
 
         <div className="todo">
-          {tasks.map((task, index) => (
-            <div className="task" key={index}>
-              <div className="task-left">
-                <div className="check-box" style={{backgroundColor: checkbox ? "var(--check-bg)" : "transparent"}}>
-                  <img src="src/images/icon-check.svg" alt="tick" />
-                </div>
-                <p>{task}</p>
-              </div>
-              
-              <div className="delete-task" onClick={handleDelete}>
-                <img src="src/images/icon-cross.svg" alt="close" />
-              </div>
-              
-            </div>
-          ))}
+        {tasks.map((task, index) => (
+  <div className="task" key={index}>
+    <div className="task-left">
+      <div className="check-box" onClick={() => handleCheckboxClick(index)} style={{backgroundColor: task.completed ? "var(--check-bg)" : "transparent"}}>
+        {task.completed && <img src="src/images/icon-check.svg" alt="tick" />}
+      </div>
+      <p className={task.completed ? 'completed' : ''}>{task.name}</p>
+    </div>
+    <div className="delete-task" onClick={() => handleDelete(index)}>
+      <img src="src/images/icon-cross.svg" alt="close" />
+    </div>
+  </div>
+))}
 
         </div>
 
