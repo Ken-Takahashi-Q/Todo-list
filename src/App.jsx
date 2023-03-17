952f8e84e7f7cd12824b320ed14370eb5f698a54
Yathip Kenichi Takahashi <ken.yathip@gmail.com>
2023-03-17 15:06:55 +0700

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
   // Dark mode
   const [darkMode, setDarkMode] = useState(true);
   const root = document.querySelector(':root');
+
+  // Set the default to dark mode
+  useEffect(() => {
+		root.dataset.theme = 'dark';
+	}, []);
 	
 	const themeChange = () => {
 		setDarkMode(!darkMode);
 		root.dataset.theme = root.dataset.theme === 'light' ? 'dark' : 'light';
 	};
 
-  // Tasks list
+  // default tasks list
   const [tasks, setTasks] = useState([
     {name: "Complete online JavaScript course", completed: false},
     {name: "Jog around the park 3x", completed: false},
@@ -20,9 +26,6 @@ function App() {
     {name: "Pick up groceries", completed: false},
     {name: "Complete Todo App on Frontend Mentor", completed: false}
   ]);
-
-  const [checkbox, setCheckbox] = useState(Array(tasks.length).fill(false));
-  const [completedTasks, setCompletedTasks] = useState([]);
 
   // add new task from input
   const handleAddTask = (e) => {
@@ -51,6 +54,47 @@ function App() {
     });
   };
   
+  // show all tasks
+  const [showAll, setShowAll] = useState(true);
+  const [showActive, setShowActive] = useState(false);
+  const [showCompleted, setShowCompleted] = useState(false);
+
+  const handleShowAll = () => {
+    setShowAll(true);
+    setShowActive(false);
+    setShowCompleted(false);
+  };
+
+  // show active
+  const handleShowActive = () => {
+    setShowAll(false);
+    setShowActive(true);
+    setShowCompleted(false);
+  };
+  
+  // show ticked
+  const handleShowCompleted = () => {
+    setShowAll(false);
+    setShowActive(false);
+    setShowCompleted(true);
+  };
+
+  // delete task completed
+  const handleClearCompleted = () => {
+    setTasks(tasks.filter(task => !task.completed));
+  };
+
+  // drag and drop (not using)
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+  
+    const items = Array.from(tasks);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+  
+    setTasks(items);
+  };
+
   return (
     <div className="App">
       <header>
@@ -72,21 +116,58 @@ function App() {
             />
         </div>
 
+        
         <div className="todo">
-        {tasks.map((task, index) => (
-          <div className="task" key={index}>
-            <div className="task-left">
-              <div className="check-box" onClick={() => handleCheckboxClick(index)} style={{backgroundColor: task.completed ? "var(--bright-blue)" : "transparent"}}>
-                {task.completed && <img src="src/images/icon-check.svg" alt="tick" />}
+          {tasks.map((task, index) => {
+            if ((showActive && task.completed) || (showCompleted && !task.completed)) {
+              return null;
+            }
+            return (
+              <div className="task" key={index}>
+                <div className="task-left">
+                  <div className="check-box"
+                    onClick={() => handleCheckboxClick(index)}
+                    style={{backgroundColor: task.completed ? "var(--bright-blue)" : "transparent"}}
+                  >
+                    {task.completed && <img src="src/images/icon-check.svg" alt="tick" />}
+                  </div>
+
+                  <p
+                    className={task.completed ? 'completed' : ''}
+                    onClick={() => handleCheckboxClick(index)}
+                  >
+                    {task.name}
+                  </p>
+                </div>
+  
+                {/* cross icon */}
+                <div className="delete-task" onClick={() => handleDelete(index)}>
+                  <img src="src/images/icon-cross.svg" alt="close" />
+                </div>
               </div>
-              <p className={task.completed ? 'completed' : ''}>{task.name}</p>
+            )
+          })}
+
+          <div className="details">
+            {/* show number of incompleted task(s) */}
+            <p>{tasks.filter(task => !task.completed).length} item{tasks.filter(task => !task.completed).length > 1 ? 's' : ''} left</p>
+
+            <div className="menus">
+              <p className="menus-text primary" onClick={handleShowAll}>All</p>
+              <p className="menus-text" onClick={handleShowActive}>Active</p>
+              <p className="menus-text" onClick={handleShowCompleted}>Completed</p>
             </div>
 
-            <div className="delete-task" onClick={() => handleDelete(index)}>
-              <img src="src/images/icon-cross.svg" alt="close" />
+            <div className="clear-completed" onClick={handleClearCompleted}>
+              <p>Clear Completed</p>
             </div>
           </div>
-        ))}
+
+          <div className="details mobile">
+            <p className="menus-text primary" onClick={handleShowAll}>All</p>
+            <p className="menus-text" onClick={handleShowActive}>Active</p>
+            <p className="menus-text" onClick={handleShowCompleted}>Completed</p>
+          </div>
 
         </div>
 
